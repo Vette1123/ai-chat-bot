095f2bd95bbfa9a72b986c62cbf2d4045486c27a
MohamedGado <boogado@yahoo.com>
2023-09-25 13:56:24 +0300

    Update page.tsx
    removed edge constant

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,7 +1,7 @@
 import { nanoid } from '@/lib/utils'
 import { Chat } from '@/components/chat/main-chat'
 
-export const runtime = 'edge'
+// export const runtime = 'edge'
 
 export default async function IndexPage() {
   const id = nanoid()
