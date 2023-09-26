55e9890b5a42efefd460b302b3112b84161f272a
Mohamed Gado <boogado@yahoo.com>
2023-09-26 20:44:19 +0300

    revert(page.tsx): remove preferd region

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import { nanoid } from '@/lib/utils'
 import { Chat } from '@/components/chat/main-chat'
 
 export const runtime = 'edge'
-export const preferredRegion = 'home'
 
 export default async function IndexPage() {
   const id = nanoid()
